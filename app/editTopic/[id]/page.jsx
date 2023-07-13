310a8cefa0cdb2de68e7d034cf3cb93dec0c2cf2
developertanvir2019 <developertanvir2019@gmail.com>
2023-07-13 11:48:11 +0600

    mongodb connecting

---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,11 @@
+import EditTopicForm from "@/app/components/EditTopicForm"
+
+const editTopic = () => {
+    return (
+        <>
+            <EditTopicForm />
+        </>
+    )
+}
+
+export default editTopic
